622242c4123f8ed6a81687a301d37c902b9aad70
cedrickmutombo05-lgtm <cedrickmutombo05@gmail.com>
2026-06-24 19:51:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 
 const express = require("express");
 const axios = require("axios");
-const { Pool } = require("pg");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const cron = require("node-cron");
 const crypto = require("crypto");
