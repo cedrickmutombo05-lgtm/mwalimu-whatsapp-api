e2fd87f77d7987185ab972703cbe1afb8690d5e1
cedrickmutombo05-lgtm <cedrickmutombo05@gmail.com>
2026-06-25 11:57:37 +0200

    Create cron.js

---
+++ src/cron.js
@@ -0,0 +1,17 @@
+
+// =========================================================
+// CRON – TÂCHES PLANIFIÉES
+// =========================================================
+
+async function executerRappels() {
+  return true;
+}
+
+async function nettoyerAnciennesDonnees() {
+  return true;
+}
+
+module.exports = {
+  executerRappels,
+  nettoyerAnciennesDonnees
+};
