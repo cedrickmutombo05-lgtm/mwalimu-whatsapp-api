29c2a10b2b9512041e9c605ef43ac8277844d9a8
cedrickmutombo05-lgtm <cedrickmutombo05@gmail.com>
2026-06-28 10:11:39 +0200

    Create index.js

---
+++ src/errors/index.js
@@ -0,0 +1,10 @@
+
+const QuotaError = require("./QuotaError");
+const NetworkError = require("./NetworkError");
+const ValidationError = require("./ValidationError");
+
+module.exports = {
+  QuotaError,
+  NetworkError,
+  ValidationError
+};
